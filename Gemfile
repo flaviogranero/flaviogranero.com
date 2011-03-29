1c6986b7ee808bc375c43630fb1eb827a8768c0d
Flavio Granero <maltempe@gmail.com>
2011-03-29 13:49:08 -0300

    heroku, ga, feedburner, github fork me, link in the title

--- Gemfile
+++ Gemfile
@@ -2,8 +2,8 @@ source 'http://rubygems.org'
 
 gem 'nesta', '0.9.4'
 gem 'coderay'
-# gem 'rack-coderay'
 gem 'rack-codehighlighter'
+gem 'heroku'
 
 
 # gem (RUBY_VERSION =~ /^1.9/) ? 'ruby-debug19': 'ruby-debug'
--- app.rb
+++ app.rb
@@ -24,9 +24,9 @@ module Nesta
 		# redirect old links with timestamp path
 		get %r{/\d{4}/\d{2}/\d{2}/([\w/.-]+)} do |page|
 		  if page =~ /filter-table-plugin/
-		    redirect "/projects/#{page}"
+		    redirect "/projects/#{page}", 301
 		  else
-		    redirect "/blog/#{page}"
+		    redirect "/blog/#{page}", 301
 		  end
 		end
   end
